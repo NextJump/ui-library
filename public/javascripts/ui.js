b8c9a3aa175cb958c172035543c0d8546985ccd0
Blacki Li Rudi Migliozzi <blacki@gmail.com>
2014-01-02 16:18:04 -0500

    added javascript and css files

---
+++ public/javascripts/ui.js
@@ -0,0 +1,33 @@
+//TODO: Make js independant of Prototype.js 
+
+nxj = window.nxj || {};
+
+nxj.ui = {};
+
+nxj.ui.sectionHead = (function() {
+  var self = {};
+
+  self.init = function() {
+    init();
+  };
+
+  function init() {
+
+  }
+
+  return self;
+}());
+
+nxj.ui.lightbox = (function() {
+  var self = {};
+
+  self.init = function() {
+    init();
+  };
+
+  function init() {
+
+  }
+
+  return self;
+}());
